263db7eafee350835416bd9c4b6745412d4ac0e7
M4RTO87 <mi.castillo875@gmail.com>
2026-05-16 22:55:34 -0400

    fix: cambiar GenerationType.AUTO a IDENTITY en Venta

--- src/main/java/com/citt/persistence/entity/Venta.java
+++ src/main/java/com/citt/persistence/entity/Venta.java
@@ -18,7 +18,7 @@ import java.time.LocalDate;
 public class Venta {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idVenta;
 
     private String direccionCompra;
